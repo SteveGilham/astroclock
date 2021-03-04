50c7b8a9a19563dae8dc3c75cc9a066fa3416ed5
Steve Gilham <SteveGilham@users.noreply.github.com>
2021-03-04 12:34:35 +0000

    Inversion skeleton

---
+++ Inversion/Inversion/Presentation/AssemblyInfo.fs
@@ -0,0 +1,19 @@
+namespace Astroclock
+open System.Reflection;
+open System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following set of attributes.
+[<assembly: AssemblyTitle("astroclock-fs")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Ravna & Tines.")>]
+[<assembly: AssemblyProduct("Planetarium clock")>]
+[<assembly: AssemblyCopyright("Copyright © Steve Gilham, 2009")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: AssemblyVersion("1.0.3369.23550")>]
+[<assembly: AssemblyFileVersion("1.0.3369.23550")>]
+()
