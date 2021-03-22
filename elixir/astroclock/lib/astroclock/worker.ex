180e437ce222abb9f8dbacb1450f273c2b2305ac
Steve Gilham <SteveGilham@users.noreply.github.com>
2021-03-22 10:09:20 +0000

    mix format

--- elixir/astroclock/lib/astroclock/worker.ex
+++ elixir/astroclock/lib/astroclock/worker.ex
@@ -2,22 +2,23 @@ defmodule Astroclock.Worker do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
-  
+
   def main() do
     :application.load(Astroclock)
+
     case Astroclock.Window.start_link() do
-        {:error, _} = error -> :io.format("Error~n~p~n", [error])
-        window -> Astroclock.Window.run(window)
+      {:error, _} = error -> :io.format("Error~n~p~n", [error])
+      window -> Astroclock.Window.run(window)
     end
   end
 
   def start_link(_) do
     case Astroclock.Window.start_link() do
-       {:error, _} = e -> e;
-        window -> {:ok, :wx_object.get_pid(window), window}
+      {:error, _} = e -> e
+      window -> {:ok, :wx_object.get_pid(window), window}
     end
   end
-  
+
   def child_spec(opts) do
     %{
       id: __MODULE__,
@@ -26,5 +27,5 @@ defmodule Astroclock.Worker do
       restart: :temporary,
       shutdown: 500
     }
-  end  
+  end
 end
