8ff529164757d6110d99a6696f342bff632f8319
Steve Gilham <SteveGilham@users.noreply.github.com>
2021-03-22 10:09:20 +0000

    mix format

--- elixir/astroclock/lib/astroclock/window.ex
+++ elixir/astroclock/lib/astroclock/window.ex
@@ -3,8 +3,8 @@ defmodule Astroclock.Window do
   @moduledoc false
   @behaviour :wx_object
 
-# Record.extract & extract_all can be used to extract records from Erlang files.
-# May need to `import` modules from the wx.hrl to replace `-import` directives
+  # Record.extract & extract_all can be used to extract records from Erlang files.
+  # May need to `import` modules from the wx.hrl to replace `-import` directives
 
   import :wx_adapter
 
@@ -12,7 +12,7 @@ defmodule Astroclock.Window do
     defstruct [:frame]
   end
 
-# API
+  # API
 
   def start() do
     :wx_object.start(__MODULE__, [], [])
@@ -21,39 +21,39 @@ defmodule Astroclock.Window do
   def start_link() do
     :wx_object.start_link(__MODULE__, [], [])
   end
-  
+
   def stop(astroclock) do
     :wx_object.stop(astroclock)
   end
-  
+
   def run(astroclock) do
     :wx_adapter.run(astroclock)
   end
 
-# object_wx callbacks
+  # object_wx callbacks
 
   @impl :wx_object
   def init(_) do
     :wx.new()
 
     frame = :wxFrame.new(:wx.null(), wxID_ANY(), "Astroclock", [])
-    
+
     :wxFrame.show(frame)
     :wxFrame.raise(frame)
 
-    { frame, %State{frame: frame} }
-
+    {frame, %State{frame: frame}}
   end
-  
+
   @impl :wx_object
   def handle_event(event, state) do
     :io.format("Unhandled Event:~n~p~n", [event])
     {:noreply, state}
   end
-  
+
   @impl :wx_object
   def handle_call(:noreply, _from, state) do
-    {:noreply, state} # wait until window closed
+    # wait until window closed
+    {:noreply, state}
   end
 
   @impl :wx_object
@@ -61,29 +61,28 @@ defmodule Astroclock.Window do
     :io.format("Unhandled Call:~n~p~n", [request])
     {:reply, :ok, state}
   end
-  
+
   @impl :wx_object
   def handle_cast(request, state) do
     :io.format("Unhandled Cast:~n~p~n", [request])
     {:noreply, state}
   end
-  
+
   @impl :wx_object
   def handle_info(info, state) do
     :io.format("Unhandled Info:~n~p~n", [info])
     {:noreply, state}
   end
-  
+
   @impl :wx_object
   def terminate(_reason, state) do
     :wxFrame.destroy(state.frame)
     :wx.destroy()
     :ok
   end
-  
+
   @impl :wx_object
   def code_change(_oldversion, state, _extra) do
     {:ok, state}
   end
-  
 end
