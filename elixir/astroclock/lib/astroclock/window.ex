c53934124ece62d80fb8b7f9758e5de582717491
Steve Gilham <SteveGilham@users.noreply.github.com>
2021-03-27 12:56:24 +0000

    init and terminate need fixing how?/why?

--- elixir/astroclock/lib/astroclock/window.ex
+++ elixir/astroclock/lib/astroclock/window.ex
@@ -14,29 +14,31 @@ defmodule Astroclock.Window do
     @moduledoc """
     UI state for `Astroclock`.
     """
-
     defstruct [:frame]
-    @type state :: %State{frame: :wxFrame}
   end
+
+
+  @type astroclock() :: :wxWindow.wxWindow()
+  @type state :: %State{frame: :wxFrame}
 
   # API
 
-  @spec start() :: {:error, any()} | {:wx_ref, any(), any(), any()}
+  @spec start() :: astroclock() | {:error, any()}
   def start do
     :wx_object.start(__MODULE__, [], [])
   end
 
-  @spec start_link() :: {:error, any()} | {:wx_ref, any(), any(), any()}
+  @spec start_link() :: astroclock() | {:error, any()}
   def start_link do
     :wx_object.start_link(__MODULE__, [], [])
   end
 
-  @spec stop(atom() | pid() | {:wx_ref, any(), any(), pid()}) :: :ok
+  @spec stop(astroclock()) :: :ok
   def stop(astroclock) do
     :wx_object.stop(astroclock)
   end
 
-  @spec run({:wx_ref, any(), any(), pid()}) :: :ok
+  @spec run(astroclock()) :: :ok
   def run(astroclock) do
     :wx_adapter.run(astroclock)
   end
@@ -44,7 +46,7 @@ defmodule Astroclock.Window do
   # object_wx callbacks
 
   @impl :wx_object
-  @spec init(any()) :: {{:wx_ref, any(), any(), any()}, any()}
+  @spec init(list()) :: {:wxFrame.wxFrame(), state()}
   def init(_) do
     :wx.new()
 
@@ -57,45 +59,41 @@ defmodule Astroclock.Window do
   end
 
   @impl :wx_object
-  @spec handle_event(
-          {:wx, integer(), {:wx_ref, any(), any(), any()}, any(), tuple()},
-          State.state()
-        ) :: {:noreply, State.state()}
+  @spec handle_event(:wx.wx(), state()):: {:noreply, state()}
   def handle_event(event, state) do
     :io.format("Unhandled Event:~n~p~n", [event])
     {:noreply, state}
   end
 
   @impl :wx_object
-  @spec handle_call(:noreply, {pid(), any()}, :junk) :: {:noreply, State.state()}
+  @spec handle_call(any(), any(), state()) :: {:reply, :ok, state()}
   def handle_call(:noreply, _from, state) do
     # wait until window closed
     {:noreply, state}
   end
 
   @impl :wx_object
-  @spec handle_call(:junk, :junk, :junk) :: {:reply, :ok, State.state()}
   def handle_call(request, _from, state) do
     :io.format("Unhandled Call:~n~p~n", [request])
     {:reply, :ok, state}
   end
 
   @impl :wx_object
-  @spec handle_cast(:junk, :junk) :: {:noreply, State.state()}
+  @spec handle_cast(any(), state()) :: {:noreply, state()}
   def handle_cast(request, state) do
     :io.format("Unhandled Cast:~n~p~n", [request])
     {:noreply, state}
   end
 
   @impl :wx_object
-  @spec handle_info(:junk, :junk) :: {:noreply, State.state()}
+  @spec handle_info(any(), state()) :: {:noreply, state()}
   def handle_info(info, state) do
     :io.format("Unhandled Info:~n~p~n", [info])
     {:noreply, state}
   end
 
   @impl :wx_object
-  @spec terminate(any(), atom() | State.state()) :: :ok
+  @spec terminate(any(), state()) :: :ok
   def terminate(_reason, state) do
     :wxFrame.destroy(state.frame)
     :wx.destroy()
@@ -103,7 +101,7 @@ defmodule Astroclock.Window do
   end
 
   @impl :wx_object
-  @spec code_change(any(), State.state(), any()) :: {:ok, State.state()}
+  @spec code_change(any(), state(), any()) :: {:ok, state()}
   def code_change(_oldversion, state, _extra) do
     {:ok, state}
   end
