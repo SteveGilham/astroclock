c9565dabbde0100c9631561f64a382b7117da366
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-10-07 14:52:51 +0100

    Stubs like this fix the build, but not the function

---
+++ astroclock.opensilver/Inversion/Inversion/AstroPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astroclock
+{
+  internal class AstroPage : System.Windows.Controls.Page
+  {
+  }
+
+  internal class AstroApp : System.Windows.Application
+  {
+  }
+}
