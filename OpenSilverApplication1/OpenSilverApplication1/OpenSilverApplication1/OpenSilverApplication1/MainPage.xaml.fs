4f580a080e994e579b4c4a7bfd5bb6cd9f437bb3
SteveGilham <SteveGilham@users.noreply.github.com>
2024-02-02 08:13:06 +0000

    re-implement the proof of concept from http://jyliao.blogspot.com/2008/11/f-and-silverlight-20.html

---
+++ OpenSilverApplication1/OpenSilverApplication1/OpenSilverApplication1/OpenSilverApplication1/MainPage.xaml.fs
@@ -0,0 +1,22 @@
+﻿namespace OpenSilverApplication1
+
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Windows
+open System.Windows.Controls
+
+type MainPage() as this =
+    inherit MainPageXaml()
+
+    // For code examples, refer to the OpenSilver Showcase app at: https://opensilver.net/gallery/
+    do
+        this.InitializeComponent()
+        // Enter construction logic here...
+        let btn = new Button(HorizontalAlignment = HorizontalAlignment.Center,
+                             VerticalAlignment = VerticalAlignment.Center)
+        btn.Content <- "Click me"
+        btn.Click.Add(fun _ ->
+                      btn.Content <- "The button has been clicked!")
+        this.Content <- btn
